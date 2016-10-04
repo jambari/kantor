09acb62de97212934ea447ff96cf10d6fcfcb723
jambari <jambari>
2016-10-04 19:16:46 +0900

    menambahkan admin site versi grappelli

--- src/si_angkasa/urls.py
+++ src/si_angkasa/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^report_builder/', include('report_builder.urls')),
 ]
